f5c23dd2a5febad7312ff2808a1a60c3780d492b
Dualvicc <vmmporci@gmail.com>
2023-03-27 13:54:32 +0200

    feat/flip in contact card and team socials implemented 27.03.2023

--- js/flip-element.js
+++ js/flip-element.js
@@ -1,4 +1,8 @@
 export default (() => {
-    
-  
+    const contactButton = document.querySelectorAll(".contactCardButton")
+    contactButton.forEach(contactButton => {
+        contactButton.addEventListener("click",()=>{
+            contactButton.closest('.contactCard').classList.toggle('active');
+        })
+    })
 })();
