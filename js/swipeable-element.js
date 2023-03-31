393bc5d2dc0bb0d156837a763db52d6ef6f4c8f0
Dualvicc <vmmporci@gmail.com>
2023-03-31 13:55:30 +0200

    Terminar modal 31.03.23

--- js/swipeable-element.js
+++ js/swipeable-element.js
@@ -1,4 +1,9 @@
 export default (() => {
-    
-  
+    const swipeButton = document.querySelectorAll('.swipeButton');
+    const modalProduct = document.querySelectorAll('.modalProduct');
+    swipeButton.forEach((button) => {
+        button.addEventListener('click', () => {
+            modalProduct.classList.toggle('active');
+        });
+    });
 })();
