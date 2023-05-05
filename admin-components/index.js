c561fdd8152e8f1ea67c418c0a6938439383acca
Victor Perez <victor.perez@kaicen.com>
2023-05-05 13:49:14 +0200

    Refactor/Estructura y Sass borrao

--- admin-components/index.js
+++
@@ -1,7 +0,0 @@
-import './header.js';
-import './search-bar.js';
-import './main.js';
-import './tabla.js';
-import './form.js';
-import './content.js';
-import './../src/modal.js';
---
+++ client/admin/src/index.js
@@ -0,0 +1,7 @@
+import './header.js';
+import './search-bar.js';
+import './main.js';
+import './tabla.js';
+import './form.js';
+import './content.js';
+import './../../front/src/modal.js';
